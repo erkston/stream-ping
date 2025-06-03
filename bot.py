42eac55865756d87efa49f642f539400c2806d4b
erkston <erkston@gmail.com>
2025-06-02 21:38:13 -0400

    Ephemeralize most messages

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import asyncio
 import datetime
 from datetime import datetime, timedelta, timezone
 from zoneinfo import ZoneInfo
-import distutils
+from setuptools._distutils.util import strtobool
 import discord
 from discord.ext import tasks
 import dotenv
@@ -94,48 +94,48 @@ async def rs(ctx, setting: discord.Option(autocomplete=discord.utils.basic_autoc
             await bot.change_presence(status=discord.Status.online,
                                       activity=discord.Activity(type=discord.ActivityType.watching,
                                                                 name=f"{BotActivity}"))
-            await ctx.respond(f'BotActivity has been set to "{BotActivity}"')
+            await ctx.respond(f'BotActivity has been set to "{BotActivity}"', ephemeral=True)
             print(f'BotGame changed to {BotActivity} by {ctx.author.display_name}')
 
         elif setting.casefold() == "allowdiscordembed":
             global AllowDiscordEmbed
             AllowDiscordEmbed = value
-            await ctx.respond(f'AllowDiscordEmbed has been set to "{AllowDiscordEmbed}"')
+            await ctx.respond(f'AllowDiscordEmbed has been set to "{AllowDiscordEmbed}"', ephemeral=True)
             print(f'AllowDiscordEmbed changed to {AllowDiscordEmbed} by {ctx.author.display_name}')
 
         elif setting.casefold() == "deleteoldalerts":
             global DeleteOldAlerts
             DeleteOldAlerts = value
-            await ctx.respond(f'DeleteOldAlerts has been set to "{DeleteOldAlerts}"')
+            await ctx.respond(f'DeleteOldAlerts has been set to "{DeleteOldAlerts}"', ephemeral=True)
             print(f'DeleteOldAlerts changed to {DeleteOldAlerts} by {ctx.author.display_name}')
 
         elif setting.casefold() == "offlinecheckinterval":
             OfflineCheckInterval = value
             OfflineCheckIntervalSeconds = convert_to_seconds(OfflineCheckInterval)
-            await ctx.respond(f'OfflineCheckInterval has been set to {OfflineCheckInterval}')
+            await ctx.respond(f'OfflineCheckInterval has been set to {OfflineCheckInterval}', ephemeral=True)
             print(f'OfflineCheckInterval changed to {OfflineCheckInterval} ({OfflineCheckIntervalSeconds}s) by {ctx.author.display_name}')
 
         elif setting.casefold() == "onlinecheckinterval":
             OnlineCheckInterval = value
             OnlineCheckIntervalSeconds = convert_to_seconds(OnlineCheckInterval)
-            await ctx.respond(f'OnlineCheckInterval has been set to {OnlineCheckInterval}')
+            await ctx.respond(f'OnlineCheckInterval has been set to {OnlineCheckInterval}', ephemeral=True)
             print(f'OnlineCheckInterval changed to {OnlineCheckInterval} ({OnlineCheckIntervalSeconds}s) by {ctx.author.display_name}')
 
         elif setting.casefold() == "alertcooldown":
             AlertCooldown = value
             AlertCooldownSeconds = convert_to_seconds(AlertCooldown)
-            await ctx.respond(f'AlertCooldown has been set to {AlertCooldown}')
+            await ctx.respond(f'AlertCooldown has been set to {AlertCooldown}', ephemeral=True)
             print(f'AlertCooldown changed to {AlertCooldown} ({AlertCooldownSeconds}s) by {ctx.author.display_name}')
 
         elif setting.casefold() == "alertadminonerror":
             global AlertAdminOnError
             AlertAdminOnError = value
-            await ctx.respond(f'AlertAdminOnError has been set to {AlertAdminOnError}')
+            await ctx.respond(f'AlertAdminOnError has been set to {AlertAdminOnError}', ephemeral=True)
             print(f'AlertAdminOnError changed to {AlertAdminOnError} by {ctx.author.display_name}')
 
         elif setting.casefold() == "twitchreauth":
             await twitch_auth()
-            await ctx.respond(f'New Twitch Auth token retrieved, expires at ' + token_expiry_time.strftime("%Y-%m-%d %H:%M:%S"))
+            await ctx.respond(f'New Twitch Auth token retrieved, expires at ' + token_expiry_time.strftime("%Y-%m-%d %H:%M:%S"), ephemeral=True)
             print(f'Twitch token renewed by {ctx.author.display_name}')
 
         elif setting.casefold() == "status":
@@ -145,14 +145,14 @@ async def rs(ctx, setting: discord.Option(autocomplete=discord.utils.basic_autoc
             for i in range(len(Streams)):
                 statusmessage.append(f'{Streams[i][0]}/{Streams[i][1]} {laststatus[i]}')
             statusmessagejoin = '\n'.join(statusmessage)
-            await ctx.respond(f'{statusmessagejoin}')
+            await ctx.respond(f'{statusmessagejoin}', ephemeral=True)
             print(f'Status queried by {ctx.author.display_name}')
 
         else:
-            await ctx.respond("I don't have that setting, please try again")
+            await ctx.respond("I don't have that setting, please try again", ephemeral=True)
             print(f'Received command from {ctx.author.display_name} but I did not understand it :(')
     else:
-        await ctx.respond('You do not have appropriate permissions! Leave me alone!!')
+        await ctx.respond('You do not have appropriate permissions! Leave me alone!!', ephemeral=True)
         print(f'Received command from {ctx.author.display_name} who does not have admin role "{bot_admin_role}"!')
 
 
@@ -261,7 +261,7 @@ async def am_i_alive():
     if (now - laststatus_time).seconds > AlertCooldownSeconds:
         print(f'amialive: Last update time ({laststatus_time.strftime("%Y-%m-%d %H:%M:%S")}) is longer ago than AlertCooldown ({AlertCooldown}), I think something may be wrong!')
         await discord_presence_on_error()
-        if distutils.util.strtobool(AlertAdminOnError):
+        if strtobool(AlertAdminOnError):
             print(f'amialive: AlertAdminOnError is {AlertAdminOnError}, sending discord message to bot admins')
             await alert_channel.send(f"\n {bot_admin_role.mention}\n I haven't been able to check Twitch in a while, please check my console!", allowed_mentions=allowed_mentions)
         else:
@@ -284,7 +284,7 @@ async def watch(stream, index):
         laststatus_time = datetime.now(timezone.utc)
         laststatus.append(f'initilized (Updated <t:' + str(int(laststatus_time.timestamp())) + ':R>)')
     print(f'watcher-{index} spawned for {stream}')
-    if distutils.util.strtobool(EnableStartupMessage):
+    if strtobool(EnableStartupMessage):
         print(f'watcher-{index}: EnableStartupMessage is {EnableStartupMessage}, sending discord message')
         await alert_channel.send(f"I'm watching to see if {stream[0]} is playing {stream[1]}")
     islive = await is_user_live(stream[0], index)
@@ -308,7 +308,7 @@ async def watch(stream, index):
             while gamematch == 1:
                 print(
                     f'watcher-{index}: {stream[0]} live and playing {stream[1]}! Sending alert (AllowDiscordEmbed = {AllowDiscordEmbed})')
-                if distutils.util.strtobool(AllowDiscordEmbed):
+                if strtobool(AllowDiscordEmbed):
                     link_string = "".join(["https://twitch.tv/", str(stream[0])])
                 else:
                     link_string = "".join(["<https://twitch.tv/", str(stream[0]), ">"])
@@ -337,7 +337,7 @@ async def is_user_live(username, index):
     except Exception as exc:
         print(f'Exception: "{exc}" while checking live status for {username}!')
         await discord_presence_on_error()
-        if distutils.util.strtobool(AlertAdminOnError):
+        if strtobool(AlertAdminOnError):
             await alert_channel.send(f'\n {bot_admin_role.mention}\n Exception occured, please check my console!',
                     allowed_mentions=allowed_mentions)
     data = response.json()['data']
@@ -360,7 +360,7 @@ async def does_game_match(stream):
     except Exception as exc:
         print(f'Exception: "{exc}" while checking game for {stream}!')
         await discord_presence_on_error()
-        if distutils.util.strtobool(AlertAdminOnError):
+        if strtobool(AlertAdminOnError):
             await alert_channel.send(f'\n {bot_admin_role.mention}\n Exception occured, please check my console!',
                     allowed_mentions=allowed_mentions)
     data = response.json()['data']
@@ -375,7 +375,7 @@ async def does_game_match(stream):
 async def delete_old_messages():
     global DeleteOldAlerts
     global OldMessagesToCheck
-    if distutils.util.strtobool(DeleteOldAlerts):
+    if strtobool(DeleteOldAlerts):
         print(f'DeleteOldAlerts is {DeleteOldAlerts}, Checking {OldMessagesToCheck} messages for old alerts to delete...')
         async for message in alert_channel.history(limit=int(OldMessagesToCheck)):
             if message.author == bot.user:
